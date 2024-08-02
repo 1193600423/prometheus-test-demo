dabec6ee6d2b56f43ca8cae3376f96572e056cac
zhao <221250061@smail.nju.edu.cn>
2024-08-02 17:47:50 +0800

    feat:接口限流 fix:端口8998更改为8080

--- src/main/java/io/daocloud/prometheustestdemo/controller/HelloController.java
+++ src/main/java/io/daocloud/prometheustestdemo/controller/HelloController.java
@@ -1,8 +1,13 @@
 package io.daocloud.prometheustestdemo.controller;
 
 import io.daocloud.prometheustestdemo.domain.Hello;
+import io.github.bucket4j.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 /**
  * @author zhaoxi
@@ -10,9 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+
+    //令牌桶，实现接口限流，每秒 100 次
+    private final Bucket bucket;
+
+    public HelloController(){
+        Bandwidth limit = Bandwidth.classic(100, Refill.greedy(100, Duration.ofSeconds(1)));
+        this.bucket = Bucket.builder()
+                .addLimit(limit)
+                .build();
+    }
+
     @GetMapping("/hello")
 //    @RequestLimit(count=100)
-    public Hello hello() {
-        return new Hello("hello");
+    public ResponseEntity<Hello> hello() {
+        if (bucket.tryConsume(1)) {
+            return ResponseEntity.ok(new Hello("hello"));
+        }
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
     }
 }
